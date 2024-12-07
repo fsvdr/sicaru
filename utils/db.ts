47c103b7972ba4a6068e275ea5417cfe970d4b69
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2024-12-06 19:11:17 -0600

    feat: Configure multi-tenant database

--- utils/db.ts
+++ utils/db.ts
@@ -1,5 +1,9 @@
-import { createClient } from '@tursodatabase/api';
+import * as schema from '@db/schema';
+import { createClient as createLibsqlClient } from '@libsql/client';
+import { createClient as createTursoClient } from '@tursodatabase/api';
+import { drizzle } from 'drizzle-orm/libsql/web';
 import md5 from 'md5';
+import { redirect } from 'next/navigation';
 import { auth } from './auth';
 
 if (!process.env.TURSO_API_TOKEN) {
@@ -10,7 +14,7 @@ if (!process.env.TURSO_ORG) {
   throw new Error('TURSO_ORG is missing');
 }
 
-export const turso = createClient({
+export const turso = createTursoClient({
   token: process.env.TURSO_API_TOKEN,
   org: process.env.TURSO_ORG,
 });
@@ -36,4 +40,36 @@ export const checkDatabaseExists = async (): Promise<boolean> => {
     console.error('[SC] Error checking database existence', error);
     return false;
   }
+};
+
+export const getDatabaseClient = async () => {
+  const url = await getLibsqlUrl();
+
+  if (!url) {
+    console.error('[SC] Failed to create database client: URL is null');
+    return redirect('/login');
+  }
+
+  try {
+    const client = createLibsqlClient({
+      url,
+      authToken: process.env.TURSO_GROUP_AUTH_TOKEN,
+    });
+
+    return drizzle(client, { schema });
+  } catch (error) {
+    console.error('[SC] Failed to create database client', error);
+    return redirect('/login');
+  }
+};
+
+const getLibsqlUrl = async (): Promise<string | null> => {
+  const dbName = await getDatabaseName();
+  const url = getDatabaseUrl(dbName);
+
+  return url ? `libsql://${url}` : null;
+};
+
+const getDatabaseUrl = (dbName: string | null): string | null => {
+  return dbName ? `${dbName}-${process.env.TURSO_ORG}.turso.io` : null;
 };
