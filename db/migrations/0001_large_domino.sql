441e173210074937a49a93fe1375beca5b1d5fc6
Luis Fernando Saavedra Meza <fernando@fsvdr.me>
2025-02-13 21:09:07 -0600

    db: Update image fields to json

---
+++ db/migrations/0001_large_domino.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "stores" ALTER COLUMN "logo" SET DATA TYPE json;--> statement-breakpoint
+ALTER TABLE "websites" ALTER COLUMN "cover_image" SET DATA TYPE json;--> statement-breakpoint
+ALTER TABLE "websites" ALTER COLUMN "favicon" SET DATA TYPE json;
